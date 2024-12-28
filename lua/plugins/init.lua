f3bb4e18758a63234e64cc56f3c27b0f154937e4
alexveretenkin <oleksii.veretionkin@inveritasoft.com>
2024-12-29 01:33:16 +0200

    Init commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,7 @@
+-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+
+return {
+  require 'plugins.gitsigns',
+  require 'plugins.which-key',
+  require 'plugins.harpoon',
+}
